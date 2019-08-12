bba353a3b615d8ea95d212e3c64fa1f2f36ece0e
Luiz Bueno <luizgrbue@hotmail.com>
2019-08-12 16:44:57 -0300

    20.5 Exercícios

--- FormCadastroConta.cs
+++ FormCadastroConta.cs
@@ -1,4 +1,5 @@
-﻿using Banco.Contas;
+﻿using Banco.Busca;
+using Banco.Contas;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,10 +16,15 @@ namespace Banco
     {
         private Form1 formPrincipal = new Form1();
         private string tipoConta;
+        private ICollection<string> devedores;
         public FormCadastroConta(Form1 formPrincipal)
         {
+
             this.formPrincipal = formPrincipal;
             InitializeComponent();
+
+            GeradorDeDevedores gerador = new GeradorDeDevedores();
+            this.devedores = gerador.GeraList();
         }
         private void BotaoCadastro_Click(object sender, EventArgs e)
         {
@@ -31,13 +37,27 @@ namespace Banco
             {
                 novaConta = new ContaPoupanca();
             }
-            else
+            else if (tipoConta == "Conta de Investimentos")
             {
                 novaConta = new ContaInvestimento();
             }
-            novaConta.Titular = new Cliente(textoCadastraTitular.Text);
-            this.formPrincipal.AdicionaConta(novaConta);
-            this.Close();
+            else
+            {
+                MessageBox.Show("Escolha um tipo de conta");
+                return;
+            }
+            string titular = textoCadastraTitular.Text;
+            novaConta.Titular = new Cliente(titular);
+            bool ehDevedor = this.devedores.Contains(titular);
+            if (!ehDevedor)
+            {
+                this.formPrincipal.AdicionaConta(novaConta);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("devedor");
+            }
         }
         private void comboTipoConta_SelectedIndexChanged(object sender, EventArgs e)
         {
